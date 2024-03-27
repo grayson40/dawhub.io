53ab95bd780b139fa054c29e227f69f78a47fe17
grayson40 <graysoncrozier40@gmail.com>
2024-03-26 21:18:17 -0400

    Remove users fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import Nav from "$lib/components/nav.svelte";
-	import Card from "$lib/components/ui/card/card.svelte";
+	// import Card from "$lib/components/ui/card/card.svelte";
 	import SideNav from "./(components)/side-nav.svelte";
 
-	export let data;
+	// export let data;
 </script>
 
 <div class="hidden md:flex flex-col">
@@ -11,14 +11,14 @@
 	<div class="grid lg:grid-cols-4">
 		<SideNav />
 		<div class="space-y-4 mt-5">
-			{#each data.users as item (item.ID)}
+			<!-- {#each data.users as item (item.ID)}
 				<Card class="p-4">
 					<div class="flex justify-between items-center py-2">
 						<h2 class="text-lg font-semibold tracking-tight">{item.Username}</h2>
 						<p class="text-sm text-gray-500">{item.Email}</p>
 					</div>
 				</Card>
-			{/each}
+			{/each} -->
 		</div>	
 	</div>
 </div>
